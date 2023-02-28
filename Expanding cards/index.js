f1b69bca8129215fa31b8a20b489ea1b9150789a
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2023-02-28 04:49:40 +0100

    ui completed

--- Expanding cards/index.js
+++ Expanding cards/index.js
@@ -1,5 +1,15 @@
 
-function multiply(a , b){
- return a * b;
-}
-document.getElementById("demo").innerHTML = multiply(10, 50);
+// function multiply(a , b){
+//  return a * b;
+// }
+// document.getElementById("demo").innerHTML = multiply(10, 50);
+
+
+//     let text = "Oyindamola, Kushimo, Maria";
+//     let prt = text.slice(6, 15)
+//     let prt1 = text.substr(5, 8)
+//     let prt2 = text.substring(6, 15)
+//     document.getElementById("stringdemo").innerHTML = prt;
+//     document.getElementById("stringdemo1").innerHTML = prt1;
+//     document.getElementById("stringdemo2").innerHTML = prt2;
+
